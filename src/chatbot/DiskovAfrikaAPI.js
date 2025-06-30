02604b0ba397c749c7d74eec18d6ad9572bb4c6e
Erickvachez <65703034+Erickvachez@users.noreply.github.com>
2025-06-30 12:10:44 +0300

    Delete src/chatbot directory

--- src/chatbot/DiskovAfrikaAPI.js
+++
@@ -1,9 +0,0 @@
-export async function getCountryCulture(country) {
-  try {
-    const res = await fetch(`https://diskovafrika.com/api/v1/country/details?name=${country}`);
-    if (!res.ok) throw new Error("API Error");
-    return await res.json();
-  } catch (error) {
-    return { error: "Failed to fetch cultural info." };
-  }
-} 
